38c4b9fc6d02908b869b0f02212b10977f4ce213
Louie Moretti <morettilouie@gmail.com>
2022-06-14 02:06:12 -0500

    pulled input data to create search

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,13 +1,29 @@
-var getUserRepos = function(user) {
+// create HTTP request
+var getUserRepos = function (user) {
     // format the gitgub api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
     // make a request to the url
-    fetch(apiUrl).then(function(response) {
-        response.json().then(function(data) {
+    fetch(apiUrl).then(function (response) {
+        response.json().then(function (data) {
             console.log(data);
         });
     });
 };
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
 
-getUserRepos();
+var formSubmitHandler = function (event) {
+    event.preventDefault();
+    // get value from input element
+    var username = nameInputEl.value.trim();
+
+    if (username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a GitHub username");
+    }
+};
+
+userFormEl.addEventListener("submit", formSubmitHandler);
